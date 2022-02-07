c68a35cd635fa3267b68348121889e582f439bd7
Aylin Gyoren <aylingyoren21@gmail.com>
2022-02-07 11:23:20 +0300

    feat: add queryClient wd react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./components/App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-  <CssBaseline>
-    <App />
-  </CssBaseline>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} position="top-right" />
+      </CssBaseline>
+    </QueryClientProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
