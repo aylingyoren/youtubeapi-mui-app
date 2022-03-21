2ccdc574d433661e644a7d2eec4b506259d09d50
aylingyoren <aylingyoren21@gmail.com>
2022-03-21 21:27:29 +0500

    fix: switch to native .env

--- public/env.js
+++
@@ -1,3 +0,0 @@
-window.env = {
-  "API_KEY": "AIzaSyDkSgCvx8NCP4IzvcJIMCxxKU6_9ZOx23U"
-};
--- src/apis/youtube.ts
+++ src/apis/youtube.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-import env from "react-dotenv";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
     part: "snippet",
     maxResults: 48,
-    key: env.API_KEY,
+    key: process.env.REACT_APP_API_KEY,
+    // key: env.API_KEY,
   },
 });
